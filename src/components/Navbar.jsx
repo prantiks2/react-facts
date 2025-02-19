83ce6992885c3abb2e4491ec5ef92cf8c1f8d03a
Prantik Saha <prantik@unifiedinfotech.net>
2025-02-19 16:22:59 +0530

    feat: created a very basic react static project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="nav-bar">
+                <img src="/src/assets/react.svg" alt="React Logo" className="nav-logo" />
+                <span className="nav-title">ReactFacts</span>
+            </nav>
+        </header>
+    );
+}
